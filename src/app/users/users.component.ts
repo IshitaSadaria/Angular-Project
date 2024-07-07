1cc63e337e79e7e0daf12eefef0e7ed8f0a23818
IshitaSadaria <ishitasadaria84308@gmail.com>
2024-07-07 23:29:06 +0530

    Initial Commit

---
+++ src/app/users/users.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
+
+@Component({
+  selector: 'app-users',
+  standalone: true,
+  imports: [],
+  templateUrl: './users.component.html',
+  styleUrl: './users.component.css'
+})
+export class UsersComponent {
+
+  selectedUSers = DUMMY_USERS[randomIndex];
+}
