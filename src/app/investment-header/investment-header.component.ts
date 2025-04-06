daa592d9d5ec97d33a52ee949dda15a2c5e29977
IshitaSadaria <ishitasadaria84308@gmail.com>
2025-04-06 23:50:46 +0530

    investment header component

---
+++ src/app/investment-header/investment-header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-investment-header',
+  templateUrl: './investment-header.component.html',
+  styleUrl: './investment-header.component.css'
+})
+export class InvestmentHeaderComponent {
+
+}
